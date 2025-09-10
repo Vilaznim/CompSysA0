23ccdbe2cff10df421e8c770331b9ad4bb75fdeb
sonil <sonil.jamshidi1@gmail.com>
2025-09-10 11:37:59 +0200

    Test

--- file.c
+++ file.c
@@ -7,12 +7,15 @@ int print_hello_world(void) {
   return fprintf(stdout, "Hello, world!\n");
 }
 
-int main(void) {
-  int retval = EXIT_SUCCESS;
+int main(int argc, char *argv[]) {
+  if (argc !=1) {
+    fprintf("Only one argument is expected. \n");
+    return EXIT_FAILURE;
+  }
 
   if (print_hello_world() <= 0) {
     retval = EXIT_FAILURE;
   }
-
+//testing now
   return retval;
 }
