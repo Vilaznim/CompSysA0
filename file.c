c50d6af024eef54b876b0917d2d4a66449be74ea
sonil <sonil.jamshidi1@gmail.com>
2025-09-17 11:04:10 +0200

    a

--- file.c
+++ file.c
@@ -10,11 +10,10 @@ int main(int argc, char *argv[]) {
   assert (argc == 2);
 
   if (fopen(argv[1], "r") == NULL) {
-    fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
+    fprintf(stderr, "Usage: file path", argv[1], strerror(errno));
     return EXIT_FAILURE;
 
   } else {
-    fprintf(stdout, "File '%s' Exists.\n", argv[1]);
     char cmd[256];
     snprintf(cmd, sizeof(cmd), "file %s", argv[1]);
     system(cmd);
