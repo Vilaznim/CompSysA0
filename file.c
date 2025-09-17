b6f301f2144e5e71426dcb8eb3073d5cabe11201
sonil <sonil.jamshidi1@gmail.com>
2025-09-17 10:36:21 +0200

    asdf

--- file.c
+++ file.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> // exit, EXIT_FAILURE, EXIT_SUCCESS.
 #include <string.h> // strerror.
 #include <errno.h>  // errno.
+#include <assert.h> // assert.
 
 //int print_hello_world(void) {
   //return fprintf(stdout, "Hello, world!\n");
@@ -13,6 +14,7 @@ int main(int argc, char *argv[]) {
   if (fopen(argv[1], "r") == NULL) {
     fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
     return EXIT_FAILURE;
+
   } else {
     fprintf(stdout, "File '%s' opened successfully.\n", argv[1]);
     return EXIT_SUCCESS;
